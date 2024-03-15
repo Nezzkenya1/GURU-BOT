d263387149da696f1966e27f6c97140516e75900
Nezzkenya1 <122768550+Nezzkenya1@users.noreply.github.com>
2024-03-15 10:46:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254101468231', 'MOGAMBI', true],
   [''] 
 ] //Number of owners
 
